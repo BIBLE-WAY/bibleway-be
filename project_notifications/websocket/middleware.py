ab27199b93f430f7f7e28115f3cf781b8a2cf8b9
Shathwik Reddy Duvvuru <shathwik30@gmail.com>
2026-02-05 22:20:25 +0530

    hbhjrb

--- project_notifications/websocket/middleware.py
+++ project_notifications/websocket/middleware.py
@@ -1,9 +1,11 @@
 from urllib.parse import parse_qs
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.tokens import UntypedToken, AccessToken
 from channels.middleware import BaseMiddleware
 from channels.db import database_sync_to_async
+import os
 
 User = get_user_model()
 
@@ -26,6 +28,26 @@ def get_user_from_token(token_string):
 
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
+        # Get origin from headers
+        headers = dict(scope.get('headers', []))
+        origin = headers.get(b'origin', b'').decode()
+
+        # Get allowed origins from CORS settings
+        allowed_origins = getattr(settings, 'CORS_ALLOWED_ORIGINS', [])
+
+        # For development, also allow localhost
+        if settings.DEBUG:
+            allowed_origins = list(allowed_origins) + ['http://localhost:5173', 'http://127.0.0.1:5173']
+
+        # Validate origin (allow if no origin or if in allowed list)
+        if origin and origin not in allowed_origins:
+            print(f"WebSocket connection rejected - Origin {origin} not in allowed origins: {allowed_origins}")
+            await send({
+                'type': 'websocket.close',
+                'code': 4403,
+            })
+            return
+
         query_string = scope.get('query_string', b'').decode()
         query_params = parse_qs(query_string)
         token = query_params.get('token', [None])[0]
